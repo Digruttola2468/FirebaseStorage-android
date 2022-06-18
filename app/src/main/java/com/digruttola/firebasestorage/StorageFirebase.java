63e5b28bc288a03bf576d1cf47f8e82ee40d1d5f
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-06-17 23:07:52 -0300

    Creando proyecto con FirebaseStorage

---
+++ app/src/main/java/com/digruttola/firebasestorage/StorageFirebase.java
@@ -0,0 +1,7 @@
+package com.digruttola.firebasestorage;
+
+import com.google.firebase.storage.FirebaseStorage;
+
+public class StorageFirebase {
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+}
