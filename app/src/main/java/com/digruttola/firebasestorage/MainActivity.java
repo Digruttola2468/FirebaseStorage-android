646593c089b0fff8c81bd1a74caa93e7fe6b05b9
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-07-14 11:28:36 -0300

    Desplazarse entre imagenes del firebaseStorage

--- app/src/main/java/com/digruttola/firebasestorage/MainActivity.java
+++ app/src/main/java/com/digruttola/firebasestorage/MainActivity.java
@@ -21,10 +21,12 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,11 +34,13 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseStorage storage = FirebaseStorage.getInstance();
     private StorageReference pathReference = storage.getReferenceFromUrl("gs://fir-storage-c7c2d.appspot.com/Horarios/horario_1b.png");
 
-    private Button btSubirImagen,btGaleria;
+    private Button btSubirImagen,btGaleria,btSiguiente;
 
     private ImageView view;
     private Uri path;
 
+
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +49,29 @@ public class MainActivity extends AppCompatActivity {
         view = findViewById(R.id.firebaseimage);
         btSubirImagen = findViewById(R.id.btSubirImagen);
         btGaleria = findViewById(R.id.btBuscarImage);
+        btSiguiente = findViewById(R.id.btSiguiente);
+
+        ArrayList<StorageReference> listadoImagenes = new ArrayList<>();
+
+        StorageReference listRef = storage.getReference().child("fotos");
+        listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for (StorageReference prefix : listResult.getPrefixes()) {
+                    // All the prefixes under listRef.
+                    // You may call listAll() recursively on them.
+                    Log.d("getPrefixes(): ",String.valueOf(prefix));
+                }
+
+                for (StorageReference item : listResult.getItems()) {
+                    // All the items under listRef.
+                    Log.d("getItems(): ",String.valueOf(item));
+                    listadoImagenes.add(item);
+                }
+            }
+        });
+
+
 
         pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
@@ -63,6 +90,35 @@ public class MainActivity extends AppCompatActivity {
                 // Handle any errors
             }
         });
+
+
+        btSiguiente.setOnClickListener( view2 -> {
+            i++;
+
+            if(i >= listadoImagenes.size()){
+                i = 0;
+            }
+
+            StorageReference storageReference = listadoImagenes.get(i);
+
+            storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    // Got the download URL for 'users/me/profile.png'
+                    Log.d("TAG",String.valueOf(uri));
+
+                    Glide.with(MainActivity.this)
+                            .load(uri)                      //URI
+                            .override(620,620)  //Dimencion
+                            .into(view);                    //ImageView
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle any errors
+                }
+            });
+        } );
 
         btGaleria.setOnClickListener( view -> {
             cargarImagenes();
