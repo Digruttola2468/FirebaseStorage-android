84f0f95bf93fcd955f638bd7476c90a893778c70
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-07-14 10:40:47 -0300

    Obteniendo imagen FirebaseStorage

--- app/src/main/java/com/digruttola/firebasestorage/MainActivity.java
+++ app/src/main/java/com/digruttola/firebasestorage/MainActivity.java
@@ -1,14 +1,55 @@
 package com.digruttola.firebasestorage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Reference to an image file in Cloud Storage
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference pathReference = storage.getReferenceFromUrl("gs://fir-storage-c7c2d.appspot.com/Horarios/horario_1b.png");
+
+    private ImageView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.firebaseimage);
+
+        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                // Got the download URL for 'users/me/profile.png'
+                Log.d("TAG",String.valueOf(uri));
+
+                Glide.with(MainActivity.this)
+                        .load(uri)                      //URI
+                        .override(620,620)  //Dimencion
+                        .into(view);                    //ImageView
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+
+
     }
 }
